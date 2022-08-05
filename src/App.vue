0beeee422219fdb3fe337216f2b558b42e8df239
batuhanOnur <batuhanonur29@gmail.com>
2022-08-05 16:53:09 +0300

    watchers

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,35 @@
     {{ refNumber }}
   </button>
 
-  <div v-for="item in state.arr" :key="item">
+  <div v-for="item in state.arr" :key="item" ref="itemRefs">
     {{ item }}
   </div>
 
   <p>Has published books:</p>
   <span>{{ publishedBooksMessage }}</span>
+  
+  <div ref="input">
+
+  </div>
 </template>
 
 <script setup>
-import { reactive, nextTick, ref, computed, onMounted, onUpdated } from 'vue';
+import { reactive, nextTick, ref, computed, onMounted, onUpdated, watch } from 'vue';
 
   // It only works for object types, It cannot hold primitive types such as string, number or boolean.
   const state = reactive({ count: 0, arr: ['foo', 'bar'] })
   // allows us to create reactive "refs" that can hold any value type 
-  const refNumber = ref(2);
+  const refNumber = ref(2); 
+  const itemRefs = ref([])
+
+  // watchers
+  const x = ref(0)
+  const input = ref(null)
+
 
   function increment() {
     state.count++;
+    x.value++;
     // state.arr.push('baz')
     nextTick(() => {
     })
@@ -64,12 +75,37 @@ import { reactive, nextTick, ref, computed, onMounted, onUpdated } from 'vue';
 
   onMounted(()=>{
     console.log('on mounted ref', refNumber.value);
+    console.log('ref', input)
+    console.log(itemRefs.value)
   })
 
   onUpdated(()=>{
     console.log('updated')
   })
 
+  // single ref watch
+  watch(x,(newX, oldX) => {
+    console.log(`old x is ${oldX} and new x is ${newX}`)
+  })
+
+  // getter for reactive
+  watch(
+    () => state.count,
+    (count) => {
+      console.log(`count is: ${count}`)
+    }
+  )
+
+/*
+  // watchEffect
+  const url = ref('https://...')
+  const data = ref(null)
+
+  watchEffect(async () => {
+    const response = await fetch(url.value)
+    data.value = await response.json()
+  })
+*/
 
 </script>
 
